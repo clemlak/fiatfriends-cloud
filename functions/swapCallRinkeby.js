5bc6ecbe452d4ec45f6e0c9f46c62aef5e68b2ac
Clement Lakhal <clementlakhal@gmail.com>
2019-03-09 23:03:02 +0100

    Updated

--- functions/swapCallRinkeby.js
+++ functions/swapCallRinkeby.js
@@ -6,19 +6,22 @@ module.exports = {
     swap: swap
 }
 
-function swap(recipientAddress, amount, ticker, currencyFrom){
-    return getPriceFeed.getPrice(currencyFrom)
-        .then((rate) => {
-            const newAmount = rate * parseFloat(amount, 10);
+function swap(recipientAddress, amount, ticker, currencyFrom) {
+  let newAmount;
 
-            if (ticker === "DAI") {
-                let tokenAddress = "0x1D329f63dbd2DfCa686a87c90D4Fe4b802F3E34D";
-                return uniswapLogic.uniswapHotSwap(recipientAddress, newAmount.toString(), tokenAddress);
-            }
-            else {
-                return ethDirectSend.sendEth(recipientAddress, newAmount.toString());
-            }
-        })
+  if (currencyFrom.toLowerCase() === 'eur') {
+    newAmount = 0.0082237 * amount;
+    console.log(newAmount);
+  } else if (currencyFrom.toLowerCase() === 'usd') {
+    newAmount = 0.0073292 * amount;
+    console.log(newAmount);
+  }
 
-
+  if (ticker === "DAI") {
+      let tokenAddress = "0x1D329f63dbd2DfCa686a87c90D4Fe4b802F3E34D";
+      return uniswapLogic.uniswapHotSwap(recipientAddress, newAmount.toString(), tokenAddress);
+  }
+  else {
+      return ethDirectSend.sendEth(recipientAddress, newAmount.toString());
+  }
 }
