bac03a987ecf102670fb047505d1c5639434b35e
Clement Lakhal <clementlakhal@gmail.com>
2019-03-09 22:38:11 +0100

    Updated

--- functions/data/priceInfo.js
+++ functions/data/priceInfo.js
@@ -5,12 +5,12 @@ module.exports = {
 }
 
 function getPrice(currencyFrom) {
-    return axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${currencyFrom}&vs_currencies=ETH`)
+    return axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=${currencyFrom.toLowerCase()}`)
         .then(response => {
             if (response.status !== 200) {
                 throw Error(response.status)
             } else {
-                return parseFloat(response.data[currencyFrom.toLowerCase()]['eth'], 10)
+                return parseFloat(response.data.ethereum[currencyFrom.toLowerCase()], 10)
             }
         })
 }
